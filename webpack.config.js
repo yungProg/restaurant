fbff920769891f557d9eb315f0e9d5326accfa2f
yungProg <122945393+yungProg@users.noreply.github.com>
2026-03-26 22:44:51 +0000

    add plugins and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
 import path from "node:path"
 
 export default {
@@ -7,5 +8,26 @@ export default {
         filename: "main.js",
         path: path.resolve(import.meta.dirname, "dist"),
         clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html",
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
 };
